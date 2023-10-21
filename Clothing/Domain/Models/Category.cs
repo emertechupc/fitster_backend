53d1ce93d64b21908145482dacc583f5324f7b6e
jrenzzo <u202118568@upc.edu.pe>
2023-10-21 15:36:47 -0500

    feat(products): implement interface repository for products

--- Clothing/Domain/Models/Category.cs
+++ Clothing/Domain/Models/Category.cs
@@ -5,4 +5,5 @@ public class Category
     public int Id { get; set; }
     public string Name { get; set; } = default!;
     public string Description { get; set; } = default!;
+    public List<Product> Products { get; set; } = new();
 }
--- Clothing/Domain/Models/Type.cs
+++ Clothing/Domain/Models/Type.cs
@@ -4,4 +4,5 @@ public class Type
 {
     public int Id { get; set; }
     public string Name { get; set; } = default!;
+    public List<Product> Products { get; set; } = new();
 }
