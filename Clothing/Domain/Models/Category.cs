bfdce1ff9083703f7e38c9a8b18aa9d7cf6a8f24
jrenzzo <u202118568@upc.edu.pe>
2023-10-21 01:29:08 -0500

    feat(products): add category domain model

---
+++ Clothing/Domain/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace Fitster.API.Clothing.Domain.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+}
