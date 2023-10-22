fb3ac5997d037f4865bb644f445052e9021d1437
jrenzzo <u202118568@upc.edu.pe>
2023-10-22 12:55:03 -0500

    chore: add several packages for backend implementation

--- Shared/Persistence/Contexts/AppDbContext.cs
+++ Shared/Persistence/Contexts/AppDbContext.cs
@@ -1,7 +1,36 @@
+using Fitster.API.Clothing.Domain.Models;
+using Leasy.API.Shared.Extensions;
+using Fitster.API.Clothing.Resources;
+
 using Microsoft.EntityFrameworkCore;
+
 namespace Fitster.API.Shared.Persistence.Contexts;
 
 public class AppDbContext: DbContext
 {
+    public DbSet<Product> Products {get; set; }
+    public DbSet<ProductDetail> ProductDetails {get; set; }
+    protected readonly IConfiguration _configuration;
+    public AppDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+    {
+        _configuration = configuration;
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        //Products
+        //Constraints
+        // builder.Entity<Product>()
+        //     .HasIndex(p => p.Name)
+        //     .IsUnique();
+
+
+
+
+
+
+
+
+    }
 
 }
