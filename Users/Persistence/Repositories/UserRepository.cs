4dbb3c94d2b2bcae67b10359d04b8049c0eeb8f2
jrenzzo <u202118568@upc.edu.pe>
2023-10-23 18:31:12 -0500

    feat(users): implement user repository layer

--- Users/Persistence/Repositories/UserRepository.cs
+++ Users/Persistence/Repositories/UserRepository.cs
@@ -13,4 +13,39 @@ public class UserRepository: BaseRepository, IUserRepository
         
     }
 
+    public async Task<IEnumerable<User>> ListAsync()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    public async Task<User> FindByIdAsync(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+    public async Task<User> FindByEmailAsync(string email)
+    {
+        return await _context.Users.SingleOrDefaultAsync(u => u.Email == email);
+    }
+
+    public bool ExistsByEmail(string email)
+    {
+        return _context.Users.Any(u => u.Email == email);
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+    }
+
+    public void Update(User user)
+    {
+        _context.Users.Update(user);
+    }
+
+    public void Remove(User user)
+    {
+        _context.Users.Remove(user);
+    }
+
 }
