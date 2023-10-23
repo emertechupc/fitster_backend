15082b76c26ce693aa28845d97a6f0786b4897c9
jrenzzo <u202118568@upc.edu.pe>
2023-10-22 23:09:07 -0500

    feat(users): add DTO for incoming and outgoing resources

---
+++ Users/Resources/UserResource.cs
@@ -0,0 +1,9 @@
+namespace Fitster.API.Clothing.Resources;
+
+public class UserResource
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+}
