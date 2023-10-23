c38a59dd47ee83be1096835f3b1bf309cfda6940
jrenzzo <u202118568@upc.edu.pe>
2023-10-22 22:37:12 -0500

    feat(users): add user domain class

---
+++ Users/Domain/Models/User.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Serialization;
+using Fitster.API.Clothing.Domain.Models;
+
+namespace Fitster.API.Users.Domain.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;    
+    public string Email { get; set; } = default!;
+    [JsonIgnore]
+    public string PasswordToken { get; set; }
+}
