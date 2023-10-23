ea5193477228ebae117c7f05ba56e5174f07d046
jrenzzo <u202118568@upc.edu.pe>
2023-10-22 20:06:01 -0500

    chore: add annotations package

---
+++ Clothing/Controllers/ProductsController.cs
@@ -0,0 +1,9 @@
+using AutoMapper;
+using Fitster.API.Clothing.Domain.Models;
+using Fitster.API.Clothing.Domain.Services;
+using Fitster.API.Clothing.Resources;
+using Fitster.API.Shared.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore;
+
+namespace Leasy.API.Reports.Controllers;
