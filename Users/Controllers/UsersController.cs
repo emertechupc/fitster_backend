616f89b087ace3c8300d4b822846589c785cf1d1
jrenzzo <u202118568@upc.edu.pe>
2023-10-23 18:46:09 -0500

    feat(users): implement controller layer in user for reports

---
+++ Users/Controllers/UsersController.cs
@@ -0,0 +1,94 @@
+using AutoMapper;
+using Fitster.API.Clothing.Resources;
+using Fitster.API.Security.Authorization.Attributes;
+using Fitster.API.Security.Domain.Services.Communication;
+using Fitster.API.Users.Domain.Models;
+using Fitster.API.Users.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Fitster.API.Users.Controllers;
+
+[Produces("application/json")]
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IMapper _mapper;
+    public UsersController(IUserService userService, IMapper mapper)
+    {
+        _userService = userService;
+        _mapper = mapper;
+    }
+    
+    [AllowAnonymous]
+    [SwaggerOperation(
+        Summary = "Records a user login",
+        Description = "Returns the data of an application user along with a hash password that identifies it.",
+        Tags = new[] {"Users"})]
+    [HttpPost("sign-in")]
+    public async Task<IActionResult> AuthenticateAsync(AuthenticateRequest request)
+    {
+        var response = await _userService.Authenticate(request);
+        return Ok(response);
+    }
+
+    [AllowAnonymous]
+    [SwaggerOperation(
+        Summary = "Registers a new user",
+        Description = "Register a new user in the database.",
+        Tags = new[] {"Users"})]
+    [HttpPost("sign-up")]
+    public async Task<IActionResult> RegisterAsync(RegisterRequest request)
+    {
+        await _userService.RegisterAsync(request);
+        return Ok(new { message = "Registration successful" });
+    }
+
+    [HttpGet]
+    [SwaggerOperation(
+        Summary = "Get All Users",
+        Description = "Get All Users already stored",
+        Tags = new[] {"Users"})]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var users = await _userService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<User>, IEnumerable<UserResource>>(users);
+        return Ok(resources);
+    }
+    
+    [HttpGet("{id}")]
+    [SwaggerOperation(
+        Summary = "Get a user by Id",
+        Description = "Get a User Data already stored",
+        Tags = new[] {"Users"})]
+    public async Task<IActionResult> GetByIdAsync(int id)
+    {
+        var user = await _userService.GetByIdAsync(id);
+        var resource = _mapper.Map<User, UserResource>(user);
+        return Ok(resource);
+    }
+    
+    [HttpPut("{id}")]
+    [SwaggerOperation(
+        Summary = "Edit a user",
+        Description = "Updates the data of a stored user given its id.",
+        Tags = new[] {"Users"})]
+    public async Task<IActionResult> UpdateAsync(int id, UpdateRequest request)
+    {
+        await _userService.UpdateAsync(id, request);
+        return Ok(new { message = "User updated successfully" });
+    }
+    
+    [HttpDelete("{id}")]
+    [SwaggerOperation(
+        Summary = "Delete a user",
+        Description = "Delete the data of a stored user given its id",
+        Tags = new[] {"Users"})]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        await _userService.DeleteAsync(id);
+        return Ok(new { message = "User deleted successfully" });
+    }   
+}
