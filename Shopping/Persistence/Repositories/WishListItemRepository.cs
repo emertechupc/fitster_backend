39b3402efa8b1d8997397884e1bd0dc048ada7d3
jrenzzo <u202118568@upc.edu.pe>
2023-10-28 23:59:46 -0500

    feat: implement wishlist repository methods

---
+++ Shopping/Persistence/Repositories/WishListItemRepository.cs
@@ -0,0 +1,55 @@
+using Fitster.API.Shopping.Domain.Models;
+using Fitster.API.Shopping.Domain.Repositories;
+using Fitster.API.Shared.Persistence.Contexts;
+using Fitster.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fitster.API.Shopping.Persistence.Repositories;
+
+public class WishListItemRepository: BaseRepository, IWishListItemRepository
+{
+    public WishListItemRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<WishListItem>> ListAsync()
+    {
+        return await _context.WishListItems.ToListAsync();
+    }
+
+    public async Task<IEnumerable<WishListItem>> ListByWishListIdAsync(int wishListId)
+    {
+        return await _context.WishListItems
+            .Where(p => p.WishListId == wishListId)
+            .Include(p => p.Product)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<WishListItem>> ListByProductIdAsync(int productId)
+    {
+        return await _context.WishListItems
+            .Where(p => p.ProductId == productId)
+            .Include(p => p.Product)
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(WishListItem wishListItem)
+    {
+        await _context.WishListItems.AddAsync(wishListItem);
+    }
+
+    public async Task<WishListItem> FindByIdAsync(int id)
+    {
+        return await _context.WishListItems.FindAsync(id);
+    }
+
+    public void Update(WishListItem wishListItem)
+    {
+        _context.WishListItems.Update(wishListItem);
+    }
+
+    public void Remove(WishListItem wishListItem)
+    {
+        _context.WishListItems.Remove(wishListItem);
+    }
+}
